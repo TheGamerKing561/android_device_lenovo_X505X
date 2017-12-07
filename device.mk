cd285999f8f89c844692e51b1474178bfb3fdb07
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    X505X: Build dummy android.hidl.[base,manager]@1.0
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>
    Signed-off-by: TheGamerKing <eyadtamer2121@gmail.com>

--- device.mk
+++ device.mk
@@ -78,5 +78,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/lenovo/X505X/X505X-vendor.mk)
